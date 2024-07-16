1c972e22206037969cf9e51a27380c82fb9df295
Luansilv16 <164948628+Luansilv16@users.noreply.github.com>
2024-07-16 06:20:16 -0300

    Update Alphabet.hx

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -37,7 +37,7 @@ class Alphabet extends FlxSpriteGroup
 	public var scaleY(default, set):Float = 1;
 	public var rows:Int = 0;
 
-	public var distancePerItem:FlxPoint = new FlxPoint(20, 100);
+	public var distancePerItem:FlxPoint = new FlxPoint(20, 120);
 	public var startPosition:FlxPoint = new FlxPoint(0, 0); //for the calculations
 
 	public function new(x:Float, y:Float, text:String = "", ?bold:Bool = true)
